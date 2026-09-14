94c3eb5c8b38ff4ad2d62b754f342950e13fe48b
sh0uv1 <mirantuten@yandex.com>
2026-09-14 09:50:08 +0000

    sngrep: 1.8.3 -> 1.8.4

--- pkgs/by-name/sn/sngrep/package.nix
+++ pkgs/by-name/sn/sngrep/package.nix
@@ -8,20 +8,20 @@
   libpcap,
   ncurses,
   openssl,
-  pcre,
+  pcre2,
   pkg-config,
   fetchpatch,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sngrep";
-  version = "1.8.3";
+  version = "1.8.4";
 
   src = fetchFromGitHub {
     owner = "irontec";
     repo = "sngrep";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-4DLbQ3OOMvJw37n3jVuztG49HlPbWrfxByi6g6AvELQ=";
+    hash = "sha256-tKIyU8W6Jvp0hoegCpOOIsJkMfEEtmfv9Se7VIQ7hVo=";
   };
 
   nativeBuildInputs = [
@@ -35,7 +35,7 @@ stdenv.mkDerivation (finalAttrs: {
     libpcap
     ncurses
     openssl
-    pcre
+    pcre2
   ];
 
   configureFlags = [
